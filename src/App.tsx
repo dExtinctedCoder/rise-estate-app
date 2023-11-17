276898b13a74d552403dda80f3fa991e20532a3b
Faithful Caleb <jamiumuhammed011@gmail.com>
2023-11-17 22:23:41 +0100

    feat: add route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Routes, Route } from "react-router-dom";
+import SplashScreen from "./page/onboarding/splashScreen.tsx";
+import ProductTourOne from "./page/onboarding/productTourOne.tsx";
+import ProductTourTwo from "./page/onboarding/productTourTwo.tsx";
+import ProductTourThree from "./page/onboarding/productTourTwo.tsx";
+import Error from "./page/error.tsx";
 
 const queryCient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryCient}>
-      <div className="app">Rise Real Estate App</div>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<SplashScreen />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/onboarding-tour-one" element={<ProductTourOne />} />
+          <Route path="/onboarding-tour-two" element={<ProductTourTwo />} />
+          <Route path="/onboarding-tour-three" element={<ProductTourThree />} />
+        </Routes>
+      </div>
     </QueryClientProvider>
   );
 }
