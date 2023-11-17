eded0e0d737d43840a8db95d7f0dfad30fdf1ba1
Faithful Caleb <jamiumuhammed011@gmail.com>
2023-11-17 22:25:16 +0100

    feat: create component to alert users on desktop mode

---
+++ src/component/desktopWarning.tsx
@@ -0,0 +1,10 @@
+const DesktopWarning = () => {
+  return (
+    <div className="hidden md:block">
+      This App is available for only mobile version currently <br />
+      Kindly switch to a mobile to use.
+    </div>
+  );
+};
+
+export default DesktopWarning;
