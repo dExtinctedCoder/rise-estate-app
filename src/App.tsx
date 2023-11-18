83705d1fd1a85ce8fe80f7844ce9dac8767c8552
Faithful Caleb <jamiumuhammed011@gmail.com>
2023-11-18 21:06:29 +0100

    feat: add routes for login pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ const Error = lazy(() => import("./page/error.tsx"));
 const Loading = lazy(() => import("./page/loading.tsx"));
 const Tour = lazy(() => import("./page/onboarding/tour.tsx"));
 const DesktopWarning = lazy(() => import("./component/desktopWarning.tsx"));
+const LoginOption = lazy(() => import("./page/authentication/loginOption.tsx"));
+const LoginForm = lazy(() => import("./page/authentication/loginForm.tsx"));
+const LoginFaq = lazy(() => import("./page/authentication/loginFaq.tsx"));
 
 const queryCient = new QueryClient();
 
@@ -20,6 +23,11 @@ function App() {
             <Route path="/onboarding-tour-one" element={<Tour index={1} />} />
             <Route path="/onboarding-tour-two" element={<Tour index={2} />} />
             <Route path="/onboarding-tour-three" element={<Tour index={3} />} />
+            <Route path="/login" Component={LoginOption}>
+              <Route path="option" Component={LoginOption} />
+              <Route path="form" Component={LoginForm} />
+              <Route path="faqs" Component={LoginFaq} />
+            </Route>
           </Routes>
           <DesktopWarning />
         </Suspense>
