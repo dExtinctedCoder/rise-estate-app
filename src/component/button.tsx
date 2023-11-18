028bc99609969faa2ad973da607ce583f66006bf
Faithful Caleb <jamiumuhammed011@gmail.com>
2023-11-18 22:56:47 +0100

    feat: add feature to allow button contain icons

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,13 +1,15 @@
 interface ButtonProps {
   text: string;
   className?: string;
+  icon?: JSX.Element;
 }
 
-const Button = ({ text, className }: ButtonProps) => {
+const Button = ({ text, className, icon }: ButtonProps) => {
   return (
     <button
-      className={`bg-[#8BC83F] text-white rounded-xl py-4 px-20 text-center font-bold tracking-[0.48px] ${className}`}
+      className={`bg-[#8BC83F] whitespace-nowrap flex items-center justify-center gap-x-2 text-white rounded-xl py-4 px-20 text-center font-bold tracking-[0.48px] ${className}`}
     >
+      {icon}
       {text}
     </button>
   );
